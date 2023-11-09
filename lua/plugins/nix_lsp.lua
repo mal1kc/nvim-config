aa2ff60b766ff8b9f380204e8e8770b1a6fe32b4
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-11-09 16:06:55 +0300

    change notif-timeout,add nil(nix lsp) to mason conditionaly

---
+++ lua/plugins/nix_lsp.lua
@@ -0,0 +1,17 @@
+-- if os is windows return empty table
+-- else return config that installs nil_ls and mason.nvim
+--
+if vim.fn.has("win32") == 1 then
+  return {}
+else
+  return {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        table.insert(opts.ensure_installed, "nil")
+      else
+        opts.ensure_installed = { "nil" }
+      end
+    end,
+  }
+end
