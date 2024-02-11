d81644e49324e7b7e136ec5a51d57565f4c37a0d
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-02-12 01:43:40 +0300

    added gitsigns (defaults) ,copilot.lua (mostly yanked from lazyextras but disabled by default)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
