2836fe65bfeae7b782604163c7b4e34d14182eff
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-08-03 16:01:33 +0300

    updt: merge font inc/dec & venv-select.nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,4 +10,27 @@ end
 if vim.g.neovide then
   vim.notify("opened in neovide loading specific settings to neovide", vim.log.levels.INFO)
   vim.o.guifont = "Iosevka Term:h14"
+
+  -- Function to update guifont
+  local function update_guifont(increment)
+    local current_size = tonumber(vim.o.guifont:match("h(%d+)"))
+    if current_size then
+      local new_size = current_size + increment
+      vim.o.guifont = "Iosevka Term:h" .. new_size
+    end
+  end
+
+  -- Ctrl -
+  vim.keymap.set("n", "<C-->", function()
+    update_guifont(1)
+  end, { noremap = true, silent = false, desc = "Increase font size (by +1)" })
+
+  -- Ctrl =
+  vim.keymap.set("n", "<C-=>", function()
+    update_guifont(-1)
+  end, { noremap = true, silent = false, desc = "Decrease font size (by -1)" })
+end
+
+if vim.fn.has("win32") == 1 then
+  vim.o.shell = "pwsh"
 end
