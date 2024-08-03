c31a76d3af16870a59e0a58b68cb636c81a4ca13
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-08-03 15:56:53 +0300

    updt: added venv-select (manual not from LazyVim python)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,8 +21,7 @@ require("lazy").setup({
 
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-
+    -- { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.lang.omnisharp" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.clangd" },
